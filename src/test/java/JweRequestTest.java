68445c0a44dbeaad5552372544d5323d9c0b3921
rushang7-eGov <rushang.dhanesha@egovernments.org>
2021-12-30 15:42:34 +0530

    Loading public and private keys form .pem files experiment with dummy keys

--- src/test/java/JweRequestTest.java
+++ src/test/java/JweRequestTest.java
@@ -1,13 +1,21 @@
 import com.nimbusds.jose.JOSEException;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.io.*;
+import java.security.KeyFactory;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,17 +25,47 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JweRequestTest {
 
-    int keysize = 2048;
     KeyPair keyPair;
     Map<String, Object> headers, payload;
     Map<String, String> encryptedObject;
 
-    @BeforeAll
-    public void initKeys() throws NoSuchAlgorithmException {
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(keysize);
-        keyPair = keyPairGenerator.generateKeyPair();
+    private String filePathToSelfSignedCertificate = "x509-self-signed-certificate.pem";
+    private String filePathToPrivateKey = "x509-private-key.pem";
 
+    private String baseURL;
+
+    @BeforeAll
+    public void init() throws NoSuchAlgorithmException, IOException, InvalidKeySpecException, CertificateException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        baseURL = classLoader.getResource("").getFile();
+
+        initKeys();
+        initContent();
+    }
+
+    private File getFile(String fileName) {
+        return new File(baseURL + File.separator + fileName);
+    }
+
+    private void initKeys() throws NoSuchAlgorithmException, IOException, InvalidKeySpecException, CertificateException {
+        FileReader fileReader = new FileReader(getFile(filePathToPrivateKey));
+        PemReader pemReader = new PemReader(fileReader);
+        PemObject pemObject = pemReader.readPemObject();
+        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(pemObject.getContent());
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) factory.generatePrivate(privateKeySpec);
+
+        fileReader = new FileReader(getFile(filePathToSelfSignedCertificate));
+        pemReader = new PemReader(fileReader);
+        pemObject = pemReader.readPemObject();
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(new ByteArrayInputStream(pemObject.getContent()));
+        RSAPublicKey publicKey = (RSAPublicKey) x509Certificate.getPublicKey();
+
+        keyPair = new KeyPair(publicKey, rsaPrivateKey);
+    }
+
+    private void initContent() {
         headers = new HashMap<>();
         headers.put("headerKey", "value");
 
@@ -40,7 +78,7 @@ class JweRequestTest {
         JweRequest jweRequest = new JweRequest(headers, payload);
         jweRequest.encryptRequest((RSAPublicKey) keyPair.getPublic());
         encryptedObject = jweRequest.getEncryptedObject();
-        System.out.println(encryptedObject.toString());
+        System.out.println("Encrypted Object: " + encryptedObject.toString());
     }
 
     @Test
@@ -50,8 +88,8 @@ class JweRequestTest {
         Map<String, Object> retrievedHeader = jweRequest.getHeaders();
         Map<String, Object> retrievedPayload = jweRequest.getPayload();
 
-        System.out.println(retrievedHeader);
-        System.out.println(retrievedPayload);
+        System.out.println("Retrieved Header: " + retrievedHeader);
+        System.out.println("Retrieved Payload:  " + retrievedPayload);
     }
 
 }
---
+++ src/test/java/key/PrivateKeyLoaderExperiment.java
@@ -0,0 +1,45 @@
+package key;
+
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class PrivateKeyLoaderExperiment {
+
+    private String filePathToPrivateKey = "x509-private-key.pem";
+    private String baseURL;
+
+    @BeforeAll
+    public void init() {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        baseURL = classLoader.getResource("").getFile();
+    }
+
+    private File getFile(String fileName) {
+        return new File(baseURL + File.separator + fileName);
+    }
+
+    @Test
+    public void loadPrivateKey() throws NoSuchAlgorithmException, IOException, InvalidKeySpecException {
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+        FileReader keyReader = new FileReader(getFile(filePathToPrivateKey));
+        PemReader pemReader = new PemReader(keyReader);
+        PemObject pemObject = pemReader.readPemObject();
+        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(pemObject.getContent());
+        RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) factory.generatePrivate(privateKeySpec);
+        System.out.println(rsaPrivateKey.toString());
+    }
+
+}
